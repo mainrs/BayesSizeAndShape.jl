e582470cd4ae2e0bc5d6721cf9064cb9100670ef
gianluca.mastrantonio <mastrantonio.gluca@gmail.com>
2023-01-11 15:53:19 +0100

    p = 2 funzionante

--- src/BayesSizeAndShape.jl
+++ src/BayesSizeAndShape.jl
@@ -1,3 +1,58 @@
 module BayesSizeAndShape
 
+
+#using Pkg
+#Pkg.activate("/Users/gianlucamastrantonio/Dropbox (Politecnico di Torino Staff)/lavori/ShapeSize/BayesSizeAndShape")
+
+##### Packages
+# using MKL
+using Distributions, Random
+using LinearAlgebra, PDMats
+using ProgressMeter
+using Kronecker
+using DataFrames
+using StatsModels
+using ToggleableAsserts
+# using ThreadsX, SparseArrays
+# using ThreadedSparseArrays
+# using SparseMatricesCSR
+# using ThreadedSparseCSR
+# # using Impute
+# # using SpecialFunctions
+
+##### Include
+include(joinpath("types.jl"))
+include(joinpath("general_functions.jl"))
+include(joinpath("mcmc.jl"))
+include(joinpath("sampler.jl"))
+# include(joinpath("func_covariance_new.jl"))
+# include(joinpath("func_project.jl"))
+# include(joinpath("new_func_project.jl"))
+# include(joinpath("func_mcmc.jl"))
+
+
+# include(joinpath("func_adapt.jl"))
+# include(joinpath("sampler.jl"))
+# include(joinpath("sampler_new.jl"))
+
+# # include(joinpath("sampler_notraset.jl")) 
+# # include(joinpath("func_adapt_notraset.jl")) 
+
+
+# include(joinpath("predictw.jl"))
+
+
+
+##### Functions
+ export
+    NoPriorBeta,
+    NoPriorSigma,
+    compute_ss_from_pre,
+    KeepReflection,
+    DoKeepReflection,
+    GeneralSigma,
+    standardize_reg,
+    mcmc
+
+
 end # module
